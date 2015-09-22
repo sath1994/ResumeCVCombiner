4d268321e883690953462bf4c843c2954f7311d3
sathu16 <Sathurshan.v@hotmail.ca>
2015-09-22 15:03:50 -0400

    First commit CV + resume combiner

---
+++ PDFCombinerApplication/PDFCombinerApplication/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace PDFCombinerApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InstructionLabel = new System.Windows.Forms.Label();
+            this.ResumeButton = new System.Windows.Forms.Button();
+            this.CombineButton = new System.Windows.Forms.Button();
+            this.CoverLetterButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // InstructionLabel
+            // 
+            this.InstructionLabel.AutoSize = true;
+            this.InstructionLabel.Location = new System.Drawing.Point(13, 13);
+            this.InstructionLabel.Name = "InstructionLabel";
+            this.InstructionLabel.Size = new System.Drawing.Size(429, 13);
+            this.InstructionLabel.TabIndex = 0;
+            this.InstructionLabel.Text = "Select the resume and then all the respective Coverletters. Out comes the combine" +
+    "d pdfs.";
+            // 
+            // ResumeButton
+            // 
+            this.ResumeButton.Location = new System.Drawing.Point(16, 29);
+            this.ResumeButton.Name = "ResumeButton";
+            this.ResumeButton.Size = new System.Drawing.Size(75, 23);
+            this.ResumeButton.TabIndex = 1;
+            this.ResumeButton.Text = "Resume";
+            this.ResumeButton.UseVisualStyleBackColor = true;
+            this.ResumeButton.Click += new System.EventHandler(this.ResumeButton_Click);
+            // 
+            // CombineButton
+            // 
+            this.CombineButton.Location = new System.Drawing.Point(444, 29);
+            this.CombineButton.Name = "CombineButton";
+            this.CombineButton.Size = new System.Drawing.Size(95, 23);
+            this.CombineButton.TabIndex = 2;
+            this.CombineButton.Text = "CombineButton";
+            this.CombineButton.UseVisualStyleBackColor = true;
+            this.CombineButton.Click += new System.EventHandler(this.CombineButton_Click);
+            // 
+            // CoverLetterButton
+            // 
+            this.CoverLetterButton.Location = new System.Drawing.Point(239, 29);
+            this.CoverLetterButton.Name = "CoverLetterButton";
+            this.CoverLetterButton.Size = new System.Drawing.Size(75, 23);
+            this.CoverLetterButton.TabIndex = 3;
+            this.CoverLetterButton.Text = "CoverLetters";
+            this.CoverLetterButton.UseVisualStyleBackColor = true;
+            this.CoverLetterButton.Click += new System.EventHandler(this.CoverLetterButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 58);
+            this.Controls.Add(this.CoverLetterButton);
+            this.Controls.Add(this.CombineButton);
+            this.Controls.Add(this.ResumeButton);
+            this.Controls.Add(this.InstructionLabel);
+            this.Name = "Form1";
+            this.Text = "Resume + CoverLetter PDF Combiner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label InstructionLabel;
+        private System.Windows.Forms.Button ResumeButton;
+        private System.Windows.Forms.Button CombineButton;
+        private System.Windows.Forms.Button CoverLetterButton;
+    }
+}
+
---
+++ PDFCombinerApplication/PDFCombinerApplication/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using iTextSharp.text.pdf;
+using System.util.collections;
+using iTextSharp.text;
+using System.Net.Mail;
+
+namespace PDFCombinerApplication
+{
+    public partial class Form1 : Form
+    {
+        private List<string> CoverLetterList = new List<string>();
+        private string ResumeStringLocation;
+        private OpenFileDialog openFileDialog = new OpenFileDialog();
+        public Form1()
+        {
+            InitializeComponent();
+            //Sets initial state
+            PDFCombineAbleState();
+        }
+
+        #region Button Event Handlers
+        private void CoverLetterButton_Click(object sender, EventArgs e)
+        {
+            //Settings for the open file dialog
+            openFileDialog.Multiselect = true;
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.Filter = "PDF Files (*.pdf)|*.pdf|All files(*.*)|*.*";
+            openFileDialog.FilterIndex = 1;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string outputMessage = "Files Selected - \n";
+                    foreach ( string filename in openFileDialog.FileNames)
+                    {
+                        //Adds to the CoverLetter list
+                        CoverLetterList.Add(filename);
+                        //Gets the real file name
+                        string realFileName = System.IO.Path.GetFileName(filename);
+                        //Adds to the output message
+                        outputMessage += CoverLetterList.Count + ". " + realFileName + "\n";
+                    }
+                    MessageBox.Show(outputMessage + " added!");
+                }
+                catch
+                {
+                    MessageBox.Show("Please Select a file!");
+                }
+
+                //Sets new state
+                PDFCombineAbleState();
+            }
+        }
+
+        private void ResumeButton_Click(object sender, EventArgs e)
+        {
+            //Settings for the open file dialog
+            openFileDialog.Multiselect = false;
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.Filter = "PDF Files (*.pdf)|*.pdf|All files(*.*)|*.*";
+            openFileDialog.FilterIndex = 1;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    ResumeStringLocation = openFileDialog.FileName;
+                    string fileName = System.IO.Path.GetFileName(ResumeStringLocation);
+                    MessageBox.Show(fileName + " added!");
+                }
+                catch
+                {
+                    MessageBox.Show("Please Select a file!");
+                }
+                
+                //Sets new state
+                PDFCombineAbleState();
+            }
+        }
+
+        private void CombineButton_Click(object sender, EventArgs e)
+        {
+            foreach (string coverletterLocation in CoverLetterList)
+            {
+                string fileLocation = Path.GetDirectoryName(coverletterLocation);
+                string filename = Path.GetFileNameWithoutExtension(coverletterLocation);
+                //CR-> Coverletter and Resume
+                string[] CR = new string[] {  coverletterLocation, ResumeStringLocation };
+                //Creates the name used for coverletter + resume
+                string SaveLocation =  Path.Combine(fileLocation,filename + " + Resume.pdf ");
+                if (!MergePDFs(CR, SaveLocation))
+                {
+                    MessageBox.Show("OHOHOH!");
+                }
+            }
+            MessageBox.Show("completed!!!!");
+            //Resets buttons
+            ResumeStringLocation = null;
+            CoverLetterList = new List<string>();
+            PDFCombineAbleState();
+        }
+        #endregion
+
+        private void PDFCombineAbleState()
+        {
+            if (ResumeStringLocation != null)
+            {
+                CoverLetterButton.Enabled = true;
+            }
+
+            if (!CheckIfListEmpty(CoverLetterList))
+            {
+                CombineButton.Enabled = true;
+            }
+
+            //If no resume or coverletters
+            if (ResumeStringLocation == null & CheckIfListEmpty(CoverLetterList))
+            {
+                CombineButton.Enabled = false;
+                CoverLetterButton.Enabled = false;
+            }
+        }
+
+        #region Helper Methods
+
+        private bool CheckIfListEmpty(List <string> listString)
+        {
+            return (listString.Count == 0);
+        }
+
+        //Code found on net
+        /// <summary>
+        /// Gets the 2 file names in the array and adds in that specific order
+        /// </summary>
+        /// <param name="fileNames"></param>
+        /// <param name="targetPdfName"></param>
+        /// <returns></returns>
+        private static bool MergePDFs(string[] fileNames, string targetPdfName)
+        {
+            bool merged = true;
+            using (FileStream stream = new FileStream(targetPdfName, FileMode.Create))
+            {
+                Document document = new Document();
+                PdfCopy pdf = new PdfCopy(document, stream);
+                PdfReader reader = null;
+                try
+                {
+                    document.Open();
+                    foreach (string file in fileNames)
+                    {
+                        reader = new PdfReader(file);
+                        pdf.AddDocument(reader);
+                        reader.Close();
+                    }
+                }
+                catch (Exception)
+                {
+                    merged = false;
+                    if (reader != null)
+                    {
+                        reader.Close();
+                    }
+                }
+                finally
+                {
+                    if (document != null)
+                    {
+                        document.Close();
+                    }
+                }
+            }
+            return merged;
+        }
+
+        #endregion
+
+
+    }
+}
